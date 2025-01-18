f4e847a2c11300deb1daacfae2a39a316ce81e19
akihito7 <akihitopro7@gmail.com>
2025-01-18 18:44:27 -0300

    fix : eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,24 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+// Extende as configurações do Next.js e TypeScript
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  
+
+  "eslint:recommended", 
+  {
+    rules: {
+      "no-console": "off", 
+      "@typescript-eslint/no-explicit-any": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "@typescript-eslint/no-wrapper-object-types": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "react/prop-types": "off", 
+      "no-unused-vars": "off",
+      "no-undef": "off", 
+    },
+  },
 ];
 
 export default eslintConfig;
